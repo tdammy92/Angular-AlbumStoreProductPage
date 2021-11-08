28964b7cde9a46f424048c04b3a4a18738636214
mercatorng <staywokewithmercator@gmail.com>
2021-11-08 13:14:19 +0100

    part4 final push

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Album } from '../album';
+import { Observable } from 'rxjs';
 
 
 @Component({
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,26 +1,22 @@
-import { Http,Response } from '@angular/http';
+import { Http, Response } from "@angular/http";
 
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
 
-import 'rxjs/add/operator/map';
+import { Observable } from "rxjs/Observable";
 
-import { Album } from './album';
+import "rxjs/add/operator/map";
 
-
+import { Album } from "./album";
 
 @Injectable()
 export class ProductService {
+  private _albumUrl = "../assets/album.json";
 
-  private _albumUrl = '../assets/album.json';
+  constructor(private _http: Http) {}
 
-  constructor(private _http:Http) { }
-
-
-  getAlbum(id:number):Observable<Album>{
-    return  this._http.get(this._albumUrl).map((response)=><Album>response.json()
-    );
-
+  getAlbum(id: number):Observable<Album> {
+    return this._http
+      .get(this._albumUrl)
+      .map(response => <Album>response.json());
   }
-
 }
